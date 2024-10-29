a18feb8c2692a31ef5bcd75426051fbaf98844fe
arsilanHussain <163583927+arsilanHussain@users.noreply.github.com>
2024-10-29 07:02:24 +0000

    Add files via upload

---
+++ project1/get_natural_features.php
@@ -0,0 +1,32 @@
+<?php
+//Get Nature Locations (Natural Features) for Nature Markers
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header("Content-Type: application/json");
+$apiEndpoint = "http://api.geonames.org/searchJSON";
+$username = "arsilanhussain"; 
+$countryCode = $_GET['country_code']; 
+if (!$countryCode) {
+    echo json_encode(["error" => "Country code is required"]);
+    exit();
+}
+$params = [
+    "country" => $countryCode,
+    "featureClass" => "T", 
+    "maxRows" => 50,
+    "username" => $username
+];
+$queryString = http_build_query($params);
+$url = "$apiEndpoint?$queryString";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+if ($response) {
+    echo $response;
+} else {
+    echo json_encode([]);
+}
+?>
