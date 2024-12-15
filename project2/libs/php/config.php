5bf93291393ac913e7f919bdc1096eea3cf4561d
arsilanHussain <163583927+arsilanHussain@users.noreply.github.com>
2024-12-15 02:08:54 +0000

    Update config.php

--- project2/libs/php/config.php
+++ project2/libs/php/config.php
@@ -1,34 +1,20 @@
 <?php
 if (!headers_sent()) {
     ob_start();
-}
+<?php
 
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+$cd_host = "127.0.0.1";
+$cd_port = 3306;
+$cd_socket = "";
 
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
-$cd_host = 'db5016845977.hosting-data.io';
-$cd_port = 3306; 
-$cd_socket = '';
-$cd_dbname = 'dbs13603156';
-$cd_user = 'dbu1307816';
-$cd_password = 'Arsilan-Hussain';
-
+$cd_dbname = "companydirectory";
+$cd_user = "root";
+$cd_password = "";
 
 $conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
-
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
-if (!headers_sent()) {
-    ob_end_clean();
-}
+?>
